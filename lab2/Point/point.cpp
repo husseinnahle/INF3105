78c33e9ba316c76b75b184e0c089dae13e1c6ab4
root <root@LAPTOP-9F4DDVAG.localdomain>
2022-03-16 20:56:18 -0400

    lab2

---
+++ lab2/Point/point.cpp
@@ -0,0 +1,39 @@
+#include <assert.h>
+#include "math.h"
+#include "point.h"
+
+Point::Point(double _x, double _y)
+    : x(_x), y(_y)
+{
+}
+Point::Point(const Point &point)
+    : x(point.x), y(point.y)
+{
+}
+Point::~Point()
+{
+}
+double Point::distance(const Point &point) const
+{
+  return sqrt(
+      pow(this->x - point.x, 2) +
+      pow(this->y - point.y, 2));
+}
+std::ostream &operator<<(std::ostream &os, const Point &point)
+{
+  os << "(" << point.x << "," << point.y << ")";
+  return os;
+}
+std::istream &operator>>(std::istream &is, Point &point)
+{
+  char po, vir, pf;
+  is >> po;
+  if (is)
+  {
+    is >> point.x >> vir >> point.y >> pf;
+    assert(po == '(');
+    assert(vir == ',');
+    assert(pf == ')');
+  }
+  return is;
+}
